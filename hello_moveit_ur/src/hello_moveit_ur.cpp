0303bbb3f3b4a56e69d2f0c817f248fe16a7bbc2
Sam Griffiths <114715155+S3942721@users.noreply.github.com>
2024-05-02 18:07:00 +1000

    Update hello_moveit_ur.cpp

--- hello_moveit_ur/src/hello_moveit_ur.cpp
+++ hello_moveit_ur/src/hello_moveit_ur.cpp
@@ -30,8 +30,8 @@ int main(int argc, char * argv[])
 
   // Define the offsets in mm
   double offset_x = 0.0;  // 100 mm
-  double offset_y = 0.1;  // 0 mm
-  double offset_z = 0.0;  // 0 mm
+  double offset_y = 0.0;  // 0 mm
+  double offset_z = 0.4;  // 0 mm
 
   // Set a target Pose
   auto target_pose = current_pose.pose;
