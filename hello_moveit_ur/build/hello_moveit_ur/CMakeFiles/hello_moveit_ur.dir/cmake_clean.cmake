44f661f499464e744bd8f879093bd456415da4bd
root <root@urbrain.vx.rmit.edu.au>
2024-05-02 02:37:45 +0000

    Working POC

---
+++ hello_moveit_ur/build/hello_moveit_ur/CMakeFiles/hello_moveit_ur.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_moveit_ur.dir/src/hello_moveit_ur.cpp.o"
+  "CMakeFiles/hello_moveit_ur.dir/src/hello_moveit_ur.cpp.o.d"
+  "hello_moveit_ur"
+  "hello_moveit_ur.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_moveit_ur.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
