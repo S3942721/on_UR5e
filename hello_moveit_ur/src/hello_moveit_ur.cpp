ce000f57fa8ec6dfbb63ee070314874dc6aecea1
Sam Griffiths <114715155+S3942721@users.noreply.github.com>
2024-05-02 14:02:44 +1000

    CP Visualise ?

--- hello_moveit_ur/src/hello_moveit_ur.cpp
+++ hello_moveit_ur/src/hello_moveit_ur.cpp
@@ -2,6 +2,7 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit_visual_tools/moveit_visual_tools.h>
 
 int main(int argc, char * argv[])
 {
@@ -16,53 +17,47 @@ int main(int argc, char * argv[])
   auto const logger = rclcpp::get_logger("hello_moveit");
 
   // Create the MoveIt MoveGroup Interface
-using moveit::planning_interface::MoveGroupInterface;
-auto move_group_interface = MoveGroupInterface(node, "ur_manipulator");  //CHANGE
+  using moveit::planning_interface::MoveGroupInterface;
+  auto move_group_interface = MoveGroupInterface(node, "ur_manipulator");  //CHANGE
 
-  // Create trajectory publisher to visualise tragectory in rviz
+  // Create MoveItVisualTools
+  auto visual_tools = std::make_shared<moveit_visual_tools::MoveItVisualTools>("base_link");
+  visual_tools->deleteAllMarkers();
+  visual_tools->loadRemoteControl();
 
-// Set a target Pose
-auto const target_pose = []{
-  geometry_msgs::msg::Pose msg;
-  msg.orientation.w = 1.0;
-  msg.position.x = -0.25;
-  msg.position.y = 0.17;
-  msg.position.z = 0.7;
-  return msg;
-}();
-move_group_interface.setPoseTarget(target_pose);
+  // Set a target Pose
+  auto const target_pose = []{
+    geometry_msgs::msg::Pose msg;
+    msg.orientation.w = 1.0;
+    msg.position.x = -0.25;
+    msg.position.y = 0.17;
+    msg.position.z = 0.7;
+    return msg;
+  }();
+  move_group_interface.setPoseTarget(target_pose);
 
-// Create a plan to that target pose
-auto const [success, plan] = [&move_group_interface]{
-  moveit::planning_interface::MoveGroupInterface::Plan msg;
-  auto const ok = static_cast<bool>(move_group_interface.plan(msg));
-  return std::make_pair(ok, msg);
-}();
+  // Create a plan to that target pose
+  auto const [success, plan] = [&move_group_interface]{
+    moveit::planning_interface::MoveGroupInterface::Plan msg;
+    auto const ok = static_cast<bool>(move_group_interface.plan(msg));
+    return std::make_pair(ok, msg);
+  }();
 
-// Execute the plan
-if(success) {
-  // Display tragectory in rviz
-  namespace rvt = rviz_visual_tools;
-  moveit_visual_tools::MoveItVisualTools visual_tools(move_group_node, "base_link", "move_group_tutorial",
-                                                      move_group.getRobotModel());
-  
-  RCLCPP_INFO(LOGGER, "Visualizing plan 1 as trajectory line");
-  visual_tools.publishAxisLabeled(target_pose1, "pose1");
-  visual_tools.publishText(text_pose, "Pose_Goal", rvt::WHITE, rvt::XLARGE);
-  visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
-  visual_tools.trigger();
-  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
-    
-  if (move_group_interface.execute(plan))
-  {
-    RCLCPP_ERROR(logger, "Execution Succeeded!!!");
+  // Visualize the plan in Rviz
+  visual_tools->publishTrajectoryLine(plan.trajectory_, move_group_interface.getRobotModel()->getJointModelGroup(move_group_interface.getName()));
+  visual_tools->trigger();
+
+  // Execute the plan
+  if(success) {
+    if (move_group_interface.execute(plan))
+    {
+      RCLCPP_ERROR(logger, "Execution Succeeded!!!");
+    } else {
+      RCLCPP_ERROR(logger, "Execution Failed!!!");
+    }
   } else {
-    RCLCPP_ERROR(logger, "Execution Failed!!!");
+    RCLCPP_ERROR(logger, "Planing failed!");
   }
-  
-} else {
-  RCLCPP_ERROR(logger, "Planing failed!");
-}
 
   // Shutdown ROS
   rclcpp::shutdown();
