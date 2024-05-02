25a82195f40d31ead77b41df83f34f610dfa8603
Sam Griffiths <114715155+S3942721@users.noreply.github.com>
2024-05-02 17:15:42 +1000

    Update hello_moveit_ur.cpp

--- hello_moveit_ur/src/hello_moveit_ur.cpp
+++ hello_moveit_ur/src/hello_moveit_ur.cpp
@@ -3,6 +3,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit_visual_tools/moveit_visual_tools.h>
+#include <iostream>
 
 int main(int argc, char * argv[])
 {
